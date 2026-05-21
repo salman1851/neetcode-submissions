7d82ec0d00c8d147df7473e6dfe87770329b0fb3
salman1851 <131760691+salman1851@users.noreply.github.com>
2026-05-21 19:04:11 +1000

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+
+        st = []
+        brack = {")":"(", "}":"{", "]":"["}
+        
+        for i, o in enumerate(s):
+
+            # append opening bracket to stack
+            if o == "(" or o == "{" or o == "[": 
+                st.append(o)
+            
+            # invalid if the top of the stack is not the mirror of the current character
+            else:
+                if st:
+                    if st.pop() != brack[o]:
+                        return False
+                else:
+                    return False
+
+        if st:
+            return False
+
+        return True
