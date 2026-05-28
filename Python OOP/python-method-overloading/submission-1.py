d4a921cd2b19aad6af75a0b64bac21d71b8654fb
salman1851 <131760691+salman1851@users.noreply.github.com>
2026-05-28 15:04:04 +1000

    Add: python-method-overloading - submission-1

---
+++ Python OOP/python-method-overloading/submission-1.py
@@ -0,0 +1,23 @@
+# from multipledispatch import dispatch
+
+class TextProcessor:
+    # Implement method overloading for format_text method
+    def format_text(self, text1: str, text2: str = None) -> str:
+        if text2 == None:
+            return text1.upper()
+        else:
+            return (text1 + text2)
+
+    # @dispatch(str, None)
+    # def format_text(self, text1: str, text2: str = None) -> str:
+    #     return text1.upper()
+
+    # @dispatch(str, str)
+    # def format_text(self, text1: str, text2: str = None) -> str:
+    #     return text1 + text2
+
+
+# Don't modify the code below
+processor = TextProcessor()
+print(processor.format_text("hello"))
+print(processor.format_text("hello", "world"))
